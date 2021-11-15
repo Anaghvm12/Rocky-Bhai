bd9c695c95e861f8218c86fd1b81ce978690063a
SangeethTS <93924598+SangeethTS@users.noreply.github.com>
2021-11-15 12:56:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,10 +10,9 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source -  https://t.me/hagayhwvwhtf 
-
+   [
+      InlineKeyboardButton('Source Code', url='https://t.me/hagayhwvwhtf')
+   ]
 <b>DEVS:</b>
 - <a href=https://t.me/DARKWEBLOAD>ᎮᏒᎧᎦᏋᏕᎧᏒ</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
