8b4591ff40cfc2d70e47a452952329b2a8e4fc0f
DARK WEBLOAD <93924598+DARK-WEBLOAD@users.noreply.github.com>
2021-12-12 10:17:37 +0530

    Create ping.py

---
+++ plugins/admemes/ping.py
@@ -0,0 +1,38 @@
+"""Telegram Ping / Pong Speed
+Syntax: .ping"""
+
+import time
+import random
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+# -- Constants -- #
+ALIVE = "വെറുതെ Alive അടിച്ചു വെറുപ്പിക്കാതട ഞൻ ഇവട ജീവനോടെ ഒക്കെ തന്നെ ണ്ട് MANH ചത്തൊന്നും പോയിട്ടില്ല🥲\n\n ⍟𝐌𝐲 𝐜𝐫𝐞𝐚𝐭𝐨𝐫: @DARKWEBLOAD\n\n⍟𝐌𝐲 𝐬𝐮𝐩𝐩𝐨𝐫𝐭: @cinemahubmoviesS\n\n⍟𝐌𝐲 𝐮𝐩𝐝𝐚𝐭𝐞𝐬: @NewOTTmoviesAll\n\n⍟𝐌𝐲 𝐬𝐮𝐩𝐩𝐨𝐫𝐭𝐞𝐫: @MagnusTG"
+HELP = "ദൈവമേ എന്നെ മാത്രം രക്ഷിക്കണേ...."
+REPO = "നമ്മൾ നമ്മൾ പോലുമറിയാതെ അധോലോകം ആയി മാറിക്കഴിഞ്ഞിരിക്കുന്നു ഷാജിയേട്ടാ..."
+# -- Constants End -- #
+
+
+@Client.on_message(filters.command("alive", COMMAND_HAND_LER) & f_onw_fliter)
+async def check_alive(_, message):
+    await message.reply_text(ALIVE)
+
+
+@Client.on_message(filters.command("help", COMMAND_HAND_LER) & f_onw_fliter)
+async def help_me(_, message):
+    await message.reply_text(HELP)
+
+
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
+
+
+@Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
+async def repo(_, message):
+    await message.reply_text(REPO)
