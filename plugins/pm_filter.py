61893489905741065bbfca1a59bf895090e07b51
Anagh VM <93310059+Anaghvm12@users.noreply.github.com>
2022-05-27 10:22:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -128,7 +128,7 @@ async def next_page(bot, query):
             [InlineKeyboardButton("⬅️ 𝖡ᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"📋 𝖯ᴀɢᴇs {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages")]
         )
     elif off_set is None:
-        btn.append([InlineKeyboardButton(f"🗓 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("NEXT »", callback_data=f"next_{req}_{key}_{n_offset}")])
+        btn.append([InlineKeyboardButton(f"🗓 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("𝖭ᴇxᴛ ➡️", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
@@ -440,8 +440,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ 𝖠ᴅᴅ 𝖬ᴇ 𝖳ᴏ 𝖸ᴏᴜʀ 𝖦ʀᴏᴜᴘs ➕', url='http://t.me/MTG_Filter_bot?startgroup=true')             
             ],[
-            InlineKeyboardButton("𝖴ᴘᴀᴅᴀᴛᴇs 📥", url='https://t.me/+wnp1SdfDIHI1ODc1'),
-            InlineKeyboardButton("𝖦ʀᴏᴜᴏ ♻️", url='https://t.me/https://t.me/Malayalam_New_Movies_Here')
+            InlineKeyboardButton("𝖴ᴘᴅᴀᴛᴇs 📥", url='https://t.me/+wnp1SdfDIHI1ODc1'),
+            InlineKeyboardButton("𝖦ʀᴏᴜᴘ ♻️", url='https://t.me/https://t.me/Malayalam_New_Movies_Here')
             ],[
             InlineKeyboardButton('𝖧ᴇʟᴘ 📚', callback_data='help'),
             InlineKeyboardButton('𝖠ʙᴏᴜᴛ 😎', callback_data='about')
@@ -827,11 +827,11 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}",
+                    text=f"📂{file.file_name}",
                     callback_data=f'files#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"📂{get_size(file.file_size)}",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -847,7 +847,7 @@ async def auto_filter(client, msg, spoll=False):
         )
     else:
         btn.append(
-            [InlineKeyboardButton(text="🗓 1/1 🗓",callback_data="pages")]
+            [InlineKeyboardButton(text="📋 1/1 📋",callback_data="pages")]
         )
     imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
     if imdb:
