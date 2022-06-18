73e83ad01e740e4574e884245d0b0b0f1aa377bb
Anagh VM <93310059+Anaghvm12@users.noreply.github.com>
2022-06-18 14:48:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -37,10 +37,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ 𝖠ᴅᴅ 𝖬ᴇ 𝖳ᴏ 𝖸ᴏᴜʀ 𝖦ʀᴏᴜᴘs ➕', url='http://t.me/MTG_Movie_Bot?startgroup=true')             
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url='http://t.me/MTG_Movie_Bot?startgroup=true')             
             ],[
-            InlineKeyboardButton('𝖧ᴇʟᴘ 📚', callback_data='help'),
-            InlineKeyboardButton('𝖠ʙᴏᴜᴛ 😎', callback_data='about')
+            InlineKeyboardButton('ʜᴇʟᴘ 📚', callback_data='help'),
+            InlineKeyboardButton('ᴀʙᴏᴜᴛ 😎', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -75,10 +75,10 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ 𝖠ᴅᴅ 𝖬ᴇ 𝖳ᴏ 𝖸ᴏᴜʀ 𝖦ʀᴏᴜᴘs ➕', url='http://t.me/MTG_Movie_Bot?startgroup=true')             
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url='http://t.me/MTG_Movie_Bot?startgroup=true')             
             ],[
-            InlineKeyboardButton('𝖧ᴇʟᴘ 📚', callback_data='help'),
-            InlineKeyboardButton('𝖠ʙᴏᴜᴛ 😎', callback_data='about')
+            InlineKeyboardButton('ʜᴇʟᴘ 📚', callback_data='help'),
+            InlineKeyboardButton('ᴀʙᴏᴜᴛ 😎', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
